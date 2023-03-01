7312c18a576fc0305421018cd4a9eb0f72903c9f
Scott <Scott@DESKTOP-RG299H4>
2023-03-01 12:45:19 -0500

    repush of the gameobject class cause the first try didn't work smh

--- Adumbration/GameObject.cs
+++ Adumbration/GameObject.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,42 @@ using System.Threading.Tasks;
 
 namespace Adumbration
 {
-    internal class GameObject
+    /// <summary>
+    /// this is the abstract 
+    /// parent class that almost EVERY
+    /// object will use 
+    /// </summary>
+    internal abstract class GameObject
     {
+        //all the protected fields that will be used in every child class
+        protected Texture2D spriteSheet;
+        protected Rectangle sourceRect;
+        protected Rectangle recPosition;
+
+        //a default constructor for this class
+        //*will change later when the spritesheet is added to make things easier
+        public GameObject()
+        {}
+
+        //all the abstract methods that will be used in all of the classes
+        
+        /// <summary>
+        /// will be changed to update anything that happens to this SPECIFIC sprite
+        /// </summary>
+        protected abstract void Update();
+
+        /// <summary>
+        /// will be changed to draw the specific object
+        /// </summary>
+        protected abstract void Draw();
+
+        /// <summary>
+        /// is here to check if a specific object is colliding with another object
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        protected abstract bool isColliding(GameObject obj);
+
+
     }
 }
