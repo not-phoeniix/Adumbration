e59dfb0fe284d72fa6ba6c341d342352c48eea91
Alexander Gough <atg9427@rit.edu>
2023-03-01 12:54:29 -0500

    Level and LevelManager

---
+++ Adumbration/Level.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adumbration
+{
+    public abstract class Level
+    {
+        // Fields
+        protected GameObject[,] tileList;
+
+        // Methods
+
+        /// <summary>
+        /// Abstract ResetLevel method to be reset a level.
+        /// The contents are in the child classes.
+        /// </summary>
+        protected abstract void ResetLevel();
+
+        /// <summary>
+        /// Abstract Update method to update the state of the game.
+        /// The contents are in the child classes.
+        /// </summary>
+        /// <param name="gameTime">State of the game's time.</param>
+        protected abstract void Update(GameTime gameTime);
+
+        /// <summary>
+        /// Abstract Draw method to draw object(s).
+        /// The contents are in the child classes.
+        /// </summary>
+        /// <param name="gameTime">State of the game's time.</param>
+        protected abstract void Draw(GameTime gameTime);
+    }
+}
---
+++ Adumbration/LevelManager.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adumbration
+{
+    /// <summary>
+    /// Creates the Levels
+    /// </summary>
+    public class LevelManager
+    {
+        // Fields
+        private Level[] allLevels;
+
+        // Methods
+
+        /// <summary>
+        /// Updates the levels
+        /// </summary>
+        /// <param name="gameTime">State of the game's time.</param>
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        /// <summary>
+        /// Draws the levels
+        /// </summary>
+        /// <param name="gameTime">State of the game's time.</param>
+        public void Draw(GameTime gameTime)
+        {
+
+        }
+    }
+}
