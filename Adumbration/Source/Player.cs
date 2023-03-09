2376a69427ffe8174374d22897b34cd4e63bc610
jaa2128 <jaa2128@rit.edu>
2023-03-09 15:53:03 -0500

    Wall Collision working correctly.

--- Adumbration/Source/Player.cs
+++ Adumbration/Source/Player.cs
@@ -22,7 +22,8 @@ namespace Adumbration
         private int windowHeight;
         private int windowWidth;
         private KeyboardState previousKbState;
-        private bool isTouchingWall;
+        private int prevX;
+        private int prevY;
 
         // Properties
         /// <summary>
@@ -52,32 +53,34 @@ namespace Adumbration
         /// </summary>
         /// <param name="gameTime">State of the game's time.</param>
         public void Update(GameTime gameTime, Level currentLevel)
-        {
+        {    
             // Player input
             KeyboardState currentKbState = Keyboard.GetState();            
 
             // Set player speed
             speed = 5;
 
-            //adds a stop int to make my(scott's) life easier while making the wall stuff
-            stop = 0;
+            // Player's current X and Y positions
+            int currentX = recPosition.X;
+            int currentY = recPosition.Y;
 
-            // Place holder until Wall class is finished
-            foreach (GameObject tile in currentLevel.TileList)
-            {
-                // If the player is touching a wall
-                if (tile is Wall && recPosition.Intersects(tile.Position))
-                {
-                    // They're touching a wall
-                    isTouchingWall = true;
-                }
-                // Otherwise
-                if (tile is Floor && recPosition.Intersects(tile.Position))
-                {
-                    // They're not
-                    isTouchingWall = false;
-                }
-            }
+            #region// Keeping this in case we need to go back to it
+            //foreach (GameObject tile in currentLevel.TileList)
+            //{
+            //    // If the player is touching a wall
+            //    if (tile is Wall && recPosition.Intersects(tile.Position))
+            //    {
+            //        // They're touching a wall
+            //        isTouchingWall = true;
+            //    }
+            //    // Otherwise
+            //    if (tile is Floor && recPosition.Intersects(tile.Position))
+            //    {
+            //        // They're not
+            //        isTouchingWall = false;
+            //    }
+            //}
+            #endregion
 
             #region// Diagonal Dashes           
             // North East
@@ -139,12 +142,24 @@ namespace Adumbration
 
                 // Keeps player in window
                 // If player is not touching a top wall let them move in that direction
-                if (recPosition.Y > 0 && !isTouchingWall)
+                if (recPosition.Y > 0)
                 {
                     recPosition.Y -= speed;
                 }
 
-            }            
+                // While moving in the North direction
+                foreach (GameObject tile in currentLevel.TileList)
+                {
+                    // If it is colliding with a wall
+                    if (tile is Wall && IsColliding(tile))
+                    {
+                        // Snap the Player to the bottom of the wall
+                        recPosition.Y = tile.Position.Height;
+                        recPosition.X = currentX;
+                    }
+                }
+
+            }
 
             // East Movement
             if (currentKbState.IsKeyDown(Keys.D))
@@ -161,6 +176,18 @@ namespace Adumbration
                 if (recPosition.X <= windowWidth - 37)
                 {
                     recPosition.X += speed;
+                }
+
+                // While moving in the East direction
+                foreach (GameObject tile in currentLevel.TileList)
+                {
+                    // if the player is colliding with a wall
+                    if (tile is Wall && IsColliding(tile))
+                    {
+                        // Snap Player to the left side of the wall
+                        recPosition.X = tile.Position.X - recPosition.Width;
+                        recPosition.Y = currentY;
+                    }
                 }
             }
 
@@ -180,6 +207,18 @@ namespace Adumbration
                 {
                     recPosition.X -= speed;
                 }
+
+                // While the player is moving in the West direction 
+                foreach (GameObject tile in currentLevel.TileList)
+                {
+                    // If the player collides with a wall
+                    if (tile is Wall && IsColliding(tile))
+                    {
+                        // Snap the player to the right side of the wall
+                        recPosition.X = tile.Position.Width;
+                        recPosition.Y = currentY;
+                    }
+                }
             }
 
             // South Movement
@@ -198,10 +237,27 @@ namespace Adumbration
                 {
                     recPosition.Y += speed;
                 }
+
+                // While moving in the South direction
+                foreach (GameObject tile in currentLevel.TileList)
+                {
+                    // If the player collides with a wall
+                    if (tile is Wall && IsColliding(tile))
+                    {
+                        // Snap player to the top of the wall
+                        recPosition.Y = tile.Position.Y;
+                        recPosition.Y = currentX;
+                    }
+                }
+
             }
             #endregion
 
             previousKbState = currentKbState;
+
+            // In case we need to use them keep them here
+            prevX = currentX;
+            prevY = currentY;
         }
 
         /// <summary>
