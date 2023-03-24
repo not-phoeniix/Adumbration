a820f7d9d9629b6e43be836f4acd568c4cd8158c
Scott <Scott@DESKTOP-RG299H4>
2023-03-24 17:20:47 -0400

    lightreceptor class being pushed for nikki to work on it with the spritesheet

---
+++ Adumbration/LightReceptor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Adumbration
+{
+    internal class LightReceptor : Wall
+    {
+        private bool activated;
+
+        //the constructor for this class
+        public LightReceptor(Texture2D spriteSheet, Rectangle sourceRect, Rectangle position)
+            : base(spriteSheet, sourceRect, position)
+        {
+            activated = false;
+        }
+
+        //checks to see if the object that is colliding with is a lightbeam
+        //if it is a lightbeam it will return as activated 
+        //otherwise it will ignore other things and stay off
+        public override bool IsColliding(GameObject obj)
+        {
+            if (obj is LightBeam)
+            {
+                return activated = true;
+            }
+            else
+            {
+                return activated = false;
+            }
+        }
+    }
+}
