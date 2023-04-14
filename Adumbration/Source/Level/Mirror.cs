3a92e930ddb9764601bdce6cac50a0ee576e6152
Alexander Gough <atg9427@rit.edu>
2023-04-14 13:50:47 -0400

    Organized Mirror and SaveStation classes

--- Adumbration/Source/Level/Mirror.cs
+++ Adumbration/Source/Level/Mirror.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 // Describes the two types of mirrors we have
 public enum MirrorType
@@ -17,7 +18,7 @@ public enum MirrorType
 
 namespace Adumbration
 {
-    internal class Mirror : GameObject
+    internal class Mirror : GameObject, IHitbox
     {
         // Fields
         private List<LightBeam> reflectedBeams;
@@ -25,8 +26,16 @@ namespace Adumbration
         private bool isLightColliding;
         private bool lightAlreadyCollided;
         private Texture2D whitePixelTexture;
+        private Rectangle hitbox;
 
-        //constructor for this class
+        // Constructor
+
+        /// <summary>
+        /// Parameterized constructor for the mirror class.
+        /// </summary>
+        /// <param name="textureDict">The dictionary to get textures from.</param>
+        /// <param name="position">The game position.</param>
+        /// <param name="type">The type of mirror.</param>
         public Mirror(Dictionary<string, Texture2D> textureDict, Rectangle position, MirrorType type)
              : base(
                    textureDict["mirror"],
@@ -43,6 +52,13 @@ namespace Adumbration
             reflectedBeams = new List<LightBeam>();
             isLightColliding = false;
             lightAlreadyCollided = false;
+
+            // Create hitbox rectangle
+            hitbox = new Rectangle(
+                position.X - 1,
+                position.Y - 1,
+                position.Width + 2,
+                position.Height + 2);
         }
 
         /// <summary>
@@ -58,6 +74,18 @@ namespace Adumbration
             get { return type; }
         }
 
+        public Rectangle Hitbox
+        {
+            get { return hitbox; }
+        }
+
+        // Methods
+
+        /// <summary>
+        /// Updates the mirror
+        /// </summary>
+        /// <param name="gameTime">State of the game's time.</param>
+        /// <param name="currentLevel">The current level.</param>
         public void Update(GameTime gameTime, Level currentLevel)
         {
             // For each light beam in the level
@@ -82,7 +110,7 @@ namespace Adumbration
         /// <summary>
         /// Creates a reflected light beam
         /// </summary>
-        /// <param name="beam">The beam that is causin the reflection</param>
+        /// <param name="incomingBeam">The beam that is causing the reflection.</param>
         public LightBeam CreateReflection(LightBeam incomingBeam)
         {
             LightBeam returnBeam = null;
@@ -158,21 +186,55 @@ namespace Adumbration
             return returnBeam;
         }
 
+        /// <summary>
+        /// Checks if a mirror and a LightBeam are colliding.
+        /// </summary>
+        /// <param name="obj">Reference to a game object.</param>
+        /// <returns>True if the collision occurs AND the object is a LightBeam, otherwise false.</returns>
+        public bool BeamIsColliding(GameObject obj)
+        {
+            return Position.Intersects(obj.Position) && obj is LightBeam;
+        }
+
+        /// <summary>
+        /// Checks for a collision between an object and the mirror.
+        /// If the object is a LightBeam,
+        /// check if the mirror's position intersects the object's.
+        /// Otherwise, check if the mirror's hitbox intersects the object's position.
+        /// </summary>
+        /// <param name="obj">Reference to a game object</param>
+        /// <returns>True if the collision occurs, otherwise false.</returns>
         public override bool IsColliding(GameObject obj)
         {
-            if (Position.Intersects(obj.Position) && obj is LightBeam)
+            if (obj is not LightBeam)
             {
-                return true;
+                return hitbox.Intersects(obj.Position);
             }
             else
             {
-                return false;
+                return BeamIsColliding(obj);
             }
         }
 
+        /// <summary>
+        /// Draw method that uses the base class's method.
+        /// </summary>
+        /// <param name="sb">SpriteBatch object to draw with.</param>
         public override void Draw(SpriteBatch sb)
         {
             base.Draw(sb);
+        }
+
+        /// <summary>
+        /// When the player and a mirror are colliding and the E key is pressed,
+        /// the player holds the mirror until the E key is pressed again.
+        /// </summary>
+        /// <param name="myPlayer">Reference to Game1's player.</param>
+        /// <param name="currentState">Current state of the keyboard.</param>
+        /// <param name="previousState">Previous state of the keyboard.</param>
+        public void Interact(Player myPlayer, KeyboardState currentState, KeyboardState previousState)
+        {
+
         }
     }
 }
